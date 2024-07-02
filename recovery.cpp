crdroidandroid/android_bootable_recovery

recovery: Drop useless version date
* Not needed as we are now showing up full
  versioning.

Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

recovery.cpp
@@ -32,7 +32,6 @@
 #include <functional>
 #include <iterator>
 #include <memory>
-#include <regex>
 #include <string>
 #include <vector>
 
@@ -812,13 +811,9 @@ Device::BuiltinAction start_recovery(Device* device, const std::vector<std::stri
   // Assume the first instance of "-[0-9]{8}-", or "-[0-9]{8}_[0-9]{6}-" in case
   // LINEAGE_VERSION_APPEND_TIME_OF_DAY is set to true has the desired date.
   std::string ver = android::base::GetProperty("ro.modversion", "");
-  std::smatch ver_date_match;
-  std::regex_search(ver, ver_date_match, std::regex("-(\\d{8}(_\\d{6})?)-"));
-  std::string ver_date = ver_date_match.str(1);  // Empty if no match.
 
   std::vector<std::string> title_lines = {
-    "Version " + android::base::GetProperty("ro.crdroid.build.version", "(unknown)") +
-        " (" + ver_date + ")",
+    "Version " + android::base::GetProperty("ro.crdroid.build.version", "(unknown)"),
   };
   if (android::base::GetBoolProperty("ro.build.ab_update", false)) {
     std::string slot = android::base::GetProperty("ro.boot.slot_suffix", "");

Evolution-XYZ/bootable_recovery

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: AnierinB <anierin@evolution-x.org>

recovery.cpp
@@ -29,7 +29,6 @@
 #include <functional>
 #include <iterator>
 #include <memory>
-#include <regex>
 #include <string>
 #include <vector>
 
@@ -830,13 +829,9 @@ Device::BuiltinAction start_recovery(Device* device, const std::vector<std::stri
   // Assume the first instance of "-[0-9]{8}-", or "-[0-9]{8}_[0-9]{6}-" in case
   // LINEAGE_VERSION_APPEND_TIME_OF_DAY is set to true has the desired date.
   std::string ver = android::base::GetProperty("ro.modversion", "");
-  std::smatch ver_date_match;
-  std::regex_search(ver, ver_date_match, std::regex("-(\\d{8}(_\\d{6})?)-"));
-  std::string ver_date = ver_date_match.str(1);  // Empty if no match.
 
   std::vector<std::string> title_lines = {
-    "Version " + android::base::GetProperty("ro.evolution.build.version", "(unknown)") +
-        " (" + ver_date + ")",
+    "Version " + android::base::GetProperty("ro.evolution.build.version", "(unknown)"),
   };
   title_lines.push_back("Product name - " + android::base::GetProperty("ro.product.device", ""));
   if (android::base::GetBoolProperty("ro.build.ab_update", false)) {